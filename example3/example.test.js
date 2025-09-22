b7ddbc3f90e6387773e05ccf6e6f9f3f02299d16
SandyFit <sandy07r@gmail.com>
2025-09-22 13:17:25 -0500

    test: add example with equality matchers

--- example3/example.test.js
+++ example3/example.test.js
@@ -1,6 +1,72 @@
-import { describe } from "node:test";
+describe('Matchers for Equality', () => {
+    test('toBe() compares using ===', () => {
+        const a = 20;
+        const b = 20;
 
-describe('Matchers for quality', () => {
+        expect(a).toBe(b);
+        expect(a).not.toBe(21);
+    });
+
+    test('toBe() compares object identity', () => {
+        const country1 = {
+            name: "Colombia",
+            population: 51_000_000,
+        }
+        const country2 = {
+            name: "Colombia",
+            population: 51_000_000,
+        }
+
+        expect(country1).toBe(country1);
+        expect(country1).not.toBe(country2);
+    });
+
+    test('toEqual() compares object property values', () => {
+        const country1 = {
+            name: "Colombia",
+            population: 51_000_000,
+        }
+        const country2 = {
+            name: "Colombia",
+            population: 51_000_000,
+        }
+
+        expect(country1).toEqual(country2);
+    });
+
+    test('toMatch() matches a string against an RE', () => {
+        const message = "Bogota is the capital of Colombia";
+
+        // ✅ Substring match
+        expect(message).toMatch("Bogota");
+
+        // ✅ Proper regex match (case insensitive, dots = wildcards)
+        expect(message).toMatch(/Bog.../i);
+
+        // ✅ Negative case
+        expect(message).not.toMatch(/Mexico/);
+
+        // ✅ This will pass because "bogota" (lowercase) doesn’t match case-sensitively
+        expect(message).not.toMatch(/bogota/);
+    });
+
+
+    test('toMatchObject() matches a object properties', () => {
+        const carOnSale = {
+            make: "Mazda",
+            model: "CX-30",
+            fuel: "hybrid",
+            color: "red",
+        };
+
+        const carDesired = {
+            make: "Mazda",
+            fuel: "hybrid",
+
+        };
+
+        expect(carOnSale).toMatchObject(carDesired);
+    });
     
 });
 
