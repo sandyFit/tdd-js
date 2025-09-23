457e47bd74c9af271c7fa24785c3f3f29777c371
SandyFit <sandy07r@gmail.com>
2025-09-23 13:31:39 -0500

    test: add Jest suite for number range matchers

--- example3/example.test.js
+++ example3/example.test.js
@@ -41,7 +41,7 @@ describe('Matchers for Equality', () => {
         expect(message).toMatch("Bogota");
 
         // ✅ Proper regex match (case insensitive, dots = wildcards)
-        expect(message).toMatch(/Bog.../i);
+        expect(message).toMatch(/bog.../i);
 
         // ✅ Negative case
         expect(message).not.toMatch(/Mexico/);
@@ -71,7 +71,66 @@ describe('Matchers for Equality', () => {
 });
 
 describe('Matchers for range', () => {
-    
+    test('toBeLessThan() test for <', () => {
+        const a = 1.5;
+        const b = 2.5;
+        expect(a).toBeLessThan(b);
+        expect(b).not.toBeLessThan(a);
+        
+        // Works for bigInt too
+        const c = 123456789987600034500532n;
+        const d = 123456789987600034500533n;
+        expect(c).toBeLessThan(d);
+        expect(d).not.toBeLessThan(d);
+    });
+
+    test('toBeLessThanOrEqual()', () => {
+        const a = 1.5;
+        expect(a).toBeLessThanOrEqual(a);
+        expect(a).toBeLessThanOrEqual(1.5);
+        expect(a).not.toBeLessThanOrEqual(1.45);
+
+        const b = 12345n;
+        expect(b).toBeLessThanOrEqual(b);
+        expect(b).toBeLessThanOrEqual(12345n);
+        expect(b).not.toBeLessThanOrEqual(12344n);
+    });
+
+    test('toBeGreaterThan()', () => {
+        const a = 1.5;
+        const b = 34;
+        expect(b).toBeGreaterThan(a);
+        expect(a).toBeGreaterThan(1);
+        expect(a).not.toBeGreaterThan(b);
+
+        const c = 12345n;
+        expect(c).toBeGreaterThan(12344n);
+        expect(c).not.toBeGreaterThan(12346n);
+    });
+
+    test('toBeGreaterThanOrEqual()', () => {
+        // First paranemeter: the number you want to compare against
+        // Second parameter: number of digits after decimal point (default 2)
+        const a = 1.5;
+        expect(a).toBeGreaterThanOrEqual(a);
+        expect(a).toBeGreaterThanOrEqual(1);
+        expect(a).not.toBeGreaterThanOrEqual(3);
+
+        const c = 12345n;
+        expect(c).toBeGreaterThanOrEqual(c);
+        expect(c).toBeGreaterThanOrEqual(12344n);
+        expect(c).not.toBeGreaterThanOrEqual(12346n);
+    });
+
+
+    test.only('toBeClose() test for mathematical proximity', () => {
+        const a = 10.12345;
+        const b = 10.12348;
+        expect(a).toBeCloseTo(b);
+        expect(a).toBeCloseTo(b, 4);
+        expect(a).not.toBeCloseTo(b, 6);
+        
+    });
 });
 
 describe('Matchers for null, undefined or NaN', () => {
